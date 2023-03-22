c37533a76b2161873000ca4cd254b081049b9e31
unknown <dacpol@K0613.waw.dacpol.eu>
2023-03-22 09:35:02 +0100

    admin configured for product_card

--- src/product_card/admin.py
+++ src/product_card/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from product_card.models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    """Creating Admin interface"""
+    list_display = ('name', 'book_author', 'publishing_year', 'pages', 'binding', \
+    'format', 'isbn', 'weight', 'age_restriction', 'date_of_addition', 'modification_date')
+
+
+    def book_author(self, obj):
+        """Printing models with M2M in Admin interface"""
+        return ", ".join([p.name + " " + p.surname for p in obj.author.all()])
+
+admin.site.register(Book, BookAdmin)
